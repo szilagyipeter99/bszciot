f4e2f18172ded79c35ad70897a3583e12c8e8b32
Péter Szilágyi <60160988+szilagyipeter99@users.noreply.github.com>
2026-03-06 11:11:21 +0100

    Upload codes

---
+++ iot_gomb/main.c
@@ -0,0 +1,57 @@
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "nvs_flash.h"
+
+#include "app_defs.h"
+#include "mqtt_connector.h"
+#include "wifi_connector.h"
+
+#define BTN_PIN GPIO_NUM_4
+
+EventGroupHandle_t my_event_group;
+ 
+static void handle_btn_task(void *params) {
+
+	gpio_config_t btn_config = {
+		.pin_bit_mask = (1ULL << BTN_PIN),
+		.mode = GPIO_MODE_INPUT,
+		.pull_up_en = true,
+	};
+	ESP_ERROR_CHECK(gpio_config(&btn_config));
+
+	bool last_btn_state = true;
+	bool curr_btn_state;
+
+	while (true) {
+		curr_btn_state = gpio_get_level(BTN_PIN);
+		if (curr_btn_state == false && last_btn_state == true) {
+			mqtt_publish_btn_pressed();
+		}
+		last_btn_state = curr_btn_state;
+		vTaskDelay(pdMS_TO_TICKS(20));
+	}
+}
+
+void app_main(void) {
+
+	esp_err_t ret = nvs_flash_init();
+
+	if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+		ESP_ERROR_CHECK(nvs_flash_erase());
+		ret = nvs_flash_init();
+	}
+
+	ESP_ERROR_CHECK(ret);
+
+	my_event_group = xEventGroupCreate();
+
+	wifi_init_phase();
+	wifi_config_phase();
+
+	ESP_ERROR_CHECK(wifi_start_phase());
+
+	ESP_ERROR_CHECK(mqtt_init());
+
+	xTaskCreate(handle_btn_task, "Handle Button", 2048, NULL, 1, NULL);
+}
